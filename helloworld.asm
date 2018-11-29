12be8508c8d91447743d56e3ac9047dcd63ea86a
José Gabriel González <jgg032899@gmail.com>
2018-11-28 20:09:48 -0400

    Comentarios agregados a helloworld.asm

--- helloworld.asm
+++ helloworld.asm
@@ -1,14 +1,14 @@
-section .data
+section .data                     ;se definen los datos
         text db "Hello World", 10 ;10 es el caracter de cambio de linea
-section .text
+section .text                     ;aqui es donde va el codigo
         global _start
 _start:
-        mov rax,1
-        mov rdi,1
-        mov rsi,text
-        mov rdx,14
+        mov rax,1     ;id de sys_write
+        mov rdi,1     ;standard ouput
+        mov rsi,text  ;se carga el texto a rsi
+        mov rdx,12    ;longitud de la cadena
         syscall       ;llamada al sistema
 
-        mov rax,60
-        mov rdi,0
+        mov rax,60    ;se termina la ejecucion del programa
+        mov rdi,0     ;no hay codigo de error
         syscall       ;llamada al sistema
